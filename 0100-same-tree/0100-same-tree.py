4e58159bccc88d6ddbc4439f6719e46caf3356a7
HyoungwooHahm <bobbyham@naver.com>
2022-10-23 15:10:50 -0500

    Time: 30 ms (96.11%), Space: 14 MB (29.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,13 +6,42 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
+        '''
         def preOrder(t1,t2):
             if t1 == None and t2 == None : return True
             if t1 == None or t2 == None or t1.val != t2.val : return False    
             return preOrder(t1.left, t2.left) and preOrder(t1.right, t2.right)
 
         return preOrder(p,q)
+        '''
+        if not p and not q : return True
+        if not p or not q : return False
+   
+
+        stack1 = [p]
+        stack2 = [q]
+        while stack1 and stack2:
+            a = stack1.pop()
+            b = stack2.pop()
+            
+            if a.val != b.val : return False
+            if a.right == None and b.right : return False
+            if b.right == None and a.right : return False
+            if a.left == None and b.left : return False
+            if b.left == None and a.left : return False
+            
+            if a.right:
+                stack1.append(a.right)
+            if a.left:
+                stack1.append(a.left)
+            if b.right:
+                stack2.append(b.right)
+            if a.left:
+                stack2.append(b.left)
+                
+        return True
+
+        
 
                 
                 
