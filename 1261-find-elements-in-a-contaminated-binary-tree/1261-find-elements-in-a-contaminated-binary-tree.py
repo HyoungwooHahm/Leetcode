ac0ad0274379177af604cca31334d248eb85b051
HyoungwooHahm <bobbyham@naver.com>
2022-10-26 20:08:57 -0500

    Time: 191 ms (45.84%), Space: 18.4 MB (23.17%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -8,20 +8,14 @@ class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
         self.check=set([0])
-        
-        root.val=0
-        queue=[root]
-        if not root : return
-        while queue:       
-            node=queue.pop(0)
-            if node.left :
-                node.left.val=node.val*2+1
-                queue.append(node.left)
-                self.check.add(node.left.val)
-            if node.right :
-                node.right.val=node.val*2+2
-                queue.append(node.right)
-                self.check.add(node.right.val)
+
+        def dfs(root, v):
+            if root:
+                root.val=v
+                self.check.add(v)
+                dfs(root.left,2*v+1)
+                dfs(root.right,2*v+2)
+        dfs(root,0)
         
        
             
